213d922a022fcc7891bc5aca56e45a95d448367c
haojiang1 <56240090+haojiang1@users.noreply.github.com>
2020-11-01 12:06:03 +0800

    Update GA.py

--- GA.py
+++ GA.py
@@ -2,7 +2,13 @@ import random
 from random import choice
 import ruleEngine
 import os
+import detection
+import img_multi_label
 
+# model, inp, transform_com = img_multi_label.init_model()
+# for i in range(100):
+#
+#     ans = img_multi_label.seach_label('G:\\ML-GCN-master\\data\\train2014\\COCO_train2014_000000000034.jpg', model, inp, transform_com)
 #define hyper parameter
 K=3
 K_POINT=2
@@ -11,26 +17,39 @@ NUMBER=11
 MAX_ITER=20
 CROSS_RATE=0.75
 MUTATION_RATE=0.1
-TYPELOSS=[5,3,1]
+TYPELOSS=[10,5,1]
 INITIAL_LIST=[0,1,4,6,7]
 OFF_POOL=[0,1,2,3,4,5,6,7,8,9,10]
-BASE_PATH='G:\\ML-GCN-master\\GA_music'
+BASE_PATH='.\\GA_music'
 #intialize population
 def initialize_pop():
     population=[]
     for k in range(COUNT):
-        population.append(INITIAL_LIST[k]*10)
+        population.append([INITIAL_LIST[k]]*10)
     return population
 
 #evaluate fitness value
 type_dict={'passion1':1,'passion-1':2, 'excited1':3, 'excited-1':4, 'happy1':5, 
-           'happy-1':6, 'relax-1':7, 'relax1':8, 'quiet-1':9, 'quiet1':10}
+           'happy-1':6, 'relaxed-1':7, 'relaxed1':8, 'quiet-1':9, 'quiet1':10}
 #fitness value: 相反大类，diff*10,不同大类，diff*5,小类，diff*1 
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    return False
+
 def parse_label(output):
-    change=int(output[-1])
-    key=output[:-1]
-    music_type=abs(type_dict[key]-change)
+    if is_number(output[-3]):
+        change = int(output[-2:])
+        key = output[:-2]
+        print(key,change)
+    else:
+        change = int(output[-1])
+        key = output[:-1]
+    music_type = abs(type_dict[key] - change)
     return music_type
 
 def parse_folder(filename):
@@ -52,11 +71,11 @@ def evaluate_pop(true_label,output_labels):
         fit_values.append(evaluate_ind(true_label,c))
     return fit_values
 
-#验证是否停止
+#evaluate if stopping criteria is satisfied
 def evaluate_stop(fit_values,iter):
-    return min(fit_values)<10 and iter<MAX_ITER
+    return min(fit_values)<=0 or iter>MAX_ITER
 
-#染色体选择，k-tournament selection method
+#chromosome selection，using k-tournament (K=3)selection method
 def tour_selection(population,fit_values):
     parents=[]
     for _ in range(COUNT):
@@ -70,6 +89,7 @@ def tour_selection(population,fit_values):
         parents.append(population[index])
     return parents
 #染色体交叉，多点交叉，K_POINT=2
+#chromosome crossover using multi-point method
 def cross(p1,p2):
     indexs=[]
     for _ in range(K_POINT):
@@ -88,41 +108,56 @@ def multi_point_crossover(parents):
     return parents
 
 #染色体突变，位翻转方式
+#chromosome mutation, using bit flip
 def mutate(offspring):
     for i in range(COUNT):
-        for j in range(NUMBER):
+        for j in range(NUMBER-1):
             r=random.random()
             if r<MUTATION_RATE:
                 old_value=offspring[i][j]
-                print(OFF_POOL,old_value)
+                # print(OFF_POOL,old_value)
                 OFF_POOL.remove(old_value)
                 new_value=choice(OFF_POOL)
                 offspring[i][j]=new_value
                 OFF_POOL.append(old_value)
     return offspring
 
+#train GA using videos
 def train():
     population=initialize_pop()
+    print('initial population:',population)
     iter=0
+    output_labels = []
     for m_type in os.listdir(BASE_PATH):
         type_path=BASE_PATH+'/'+m_type
         for degree in os.listdir(type_path):
             filepath=type_path+'/'+degree
             print(degree)
             true_label=parse_folder(degree)
-            #rule engine
-            output_labels=[]
-            for c in population:
-                print(filepath)
-                output=ruleEngine.find_image_label(filepath,c)
-                output_labels.append(parse_label(output))
-            fit_values=evaluate_pop(true_label,output_labels)
-            if evaluate_stop(fit_values,iter):
-                return population
-            parents=tour_selection(population,fit_values)
-            offspring=multi_point_crossover(parents)
-            population=mutate(offspring)
-            iter+=1
+
+            for videoname in os.listdir(filepath):
+                output_labels = []
+                upload=filepath+'/'+videoname
+                image_path = detection.extract_frame(upload)
+                for c in population:
+                    output=ruleEngine.find_image_label(image_path,c)
+                    if not is_number(output[-2]):
+                        break
+                    print(output)
+                    output_labels.append(parse_label(output))
+                print('output labels', output_labels)
+                if output_labels==[]:
+                    print('no output label!')
+                    continue
+                fit_values=evaluate_pop(true_label,output_labels)
+                print('fitness value:', fit_values)
+                if evaluate_stop(fit_values,iter):
+                    return population
+                parents=tour_selection(population,fit_values)
+                offspring=multi_point_crossover(parents)
+                population=mutate(offspring)
+                print(f"{iter} iteration:",population)
+                iter+=1
     return population
 
-print(train())
+# print(train())
